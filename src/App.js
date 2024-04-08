3907a56348142bc86a11fac435da9ef99828f598
Himanshu Raj <himanshuraj6771@gmail.com>
2024-04-08 19:36:43 +0530

    route initaialisation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/home';
+import Signin from './Pages/signin';
+import Signup from './Pages/signup';
+import Dashboard from './Pages/dashboard';
+import Profile from './Pages/profile';
 function App() {
   return (
     <div className="App">
-      <h1>My name is Himanshu Raj</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
